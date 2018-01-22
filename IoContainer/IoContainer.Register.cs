c8f60d2d5156a7119c0b3b20d79b96a41f4d44ac
Kosiso <kosiso.gaxion@gmail.com>
2018-01-22 09:58:44 +0100

    custom Iocontainer

---
+++ IoContainer/IoContainer.Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoContainer
+{
+    public static class IoContainer
+    {
+        public static void Register<TContract, TImplementation>()
+        {
+            
+        }
+    }
+}
---
+++ IoContainer/MDLog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoContainer
+{
+    public static class MDLog
+    {
+        private static readonly IDictionary<Type, Type> types = new Dictionary<Type, Type>();
+        private static readonly IDictionary<Type, object> typeInstance = new Dictionary<Type, object>();
+
+        public static void Register<TService, TServiceImpl>()
+        {
+            types[typeof(TService)] = typeof(TServiceImpl);
+        }
+
+        public static void Register<TService, TServiceImpl>(TServiceImpl instances)
+        {
+            typeInstance[typeof(TService)] = instances;
+        }
+
+       
+        public static T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        public static object Resolve(Type service)
+        {
+           if (typeInstance.ContainsKey(service))
+            {
+                return typeInstance[service];
+            }
+
+            Type _ServiceImplementation = types[service];
+            ConstructorInfo constructors = _ServiceImplementation.GetConstructors()[0];
+            ParameterInfo[] _constructorparameter = constructors.GetParameters();
+            if(_constructorparameter.Length == 0)
+            {
+                return Activator.CreateInstance(_ServiceImplementation);
+            }
+
+            List<object> _parameter = new List<object>(_constructorparameter.Length);
+            foreach(ParameterInfo _param in _constructorparameter)
+            {
+                _parameter.Add(Resolve(_param.ParameterType));
+            }
+
+            return constructors.Invoke(_parameter.ToArray());
+        }
+
+    }
+}
